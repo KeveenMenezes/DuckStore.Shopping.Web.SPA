257eeeb442baa80a288c6b37daf35323a4466d57
Keveen Menezes <endykeven@gmail.com>
2025-05-29 17:24:57 -0300

    feat: adiciona interceptor de carregamento e serviço Busy para gerenciar estados de carregamento na aplicação

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { delay, finalize } from 'rxjs';
+import { BusyService } from '../services/busy.service';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const busyService = inject(BusyService);
+
+  busyService.busy();
+
+  return next(req).pipe(
+    delay(500),
+    finalize(() => busyService.idle())
+  );
 };
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MatIconModule,
-    MatButtonModule,
     MatBadgeModule,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatProgressBarModule
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  busyService = inject(BusyService);
 
 }
